ef97a16a2d670dbd0f03b8d7216061c6178ce857
Stephanie <stephcshea@gmail.com>
2018-06-22 17:05:08 -0400

    database working

--- Program.cs
+++ Program.cs
@@ -1,35 +1,28 @@
-﻿using System;
+﻿//from  tutorial https://docs.microsoft.com/en-us/ef/core/get-started/netcore/new-db-sqlite
+
+using System;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 namespace ConsoleApp.SQLite
 {
-    public class BloggingContext : DbContext
+    public class Program
     {
-        public DbSet<Blog> Blogs { get; set; }
-        public DbSet<Post> Posts { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public static void Main()
         {
-            optionsBuilder.UseSqlite("Data Source=blogging.db");
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
         }
-    }
-
-    public class Blog
-    {
-        public int BlogId { get; set; }
-        public string Url { get; set; }
-
-        public List<Post> Posts { get; set; }
-    }
-
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-        public int BlogId { get; set; }
-        public Blog Blog { get; set; }
     }
 }
